89b212a64bb3561a495edafb1986bcba0e640d9d
Mervin <mervinmervin0660@gmail.com>
2026-08-11 18:42:31 +0530

    the change1

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Stage1 from "../components/Stage1Component/Stage1";
 import Stage2 from "../components/Stage2Component/Stage2";
-import PrivateRouter from "./PrivateRouter/PrivateRouter";
 import SubmittedSuccess from "../Submited";
 
 const router = createBrowserRouter([
